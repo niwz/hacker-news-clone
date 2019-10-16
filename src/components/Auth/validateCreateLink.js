542cc2eb052e7521a45e35569bf8c522a5772cd7
Nicholas Wong <nic.wzx@gmail.com>
2019-10-15 20:53:25 -0700

    Add voting functionality

--- src/components/Auth/validateCreateLink.js
+++ src/components/Auth/validateCreateLink.js
@@ -1,1 +1,17 @@
-export default function validateCreateLink(values) {}
+export default function validateCreateLink(values) {
+    let errors = {}
+
+    if (!values.description) {
+        errors.description = "Description required"
+    } else if (values.description.length < 10) {
+        errors.description = "Description is too short."
+    }
+
+    if (!values.url) {
+        errors.url = "URL required"
+    } else if (!/^(ftp|http|https):\/\/[^ "]+$/.test(values.url)) {
+        errors.url = "Invalid URL."
+    }
+
+    return errors;
+}
--- src/components/Link/LinkList.js
+++ src/components/Link/LinkList.js
@@ -1,10 +1,40 @@
-import React from "react";
-import useAuth from '../Auth/useAuth';
+import React, { useContext, useEffect, useState } from "react";
+import FirebaseContext from '../../firebase/context';
+import LinkItem from './LinkItem';
 
 function LinkList(props) {
-  const user = useAuth()
-  console.log({ user })
-  return <div>LinkList</div>;
+  const { firebase } = useContext(FirebaseContext)
+  const [links, setLinks] = useState([])
+  useEffect(() => {
+    getLinks();
+  }, [])
+  const isNewPage = props.location.pathname.includes('new');
+
+  function getLinks() {
+    firebase.db.collection('links').orderBy('created', 'desc').onSnapshot(handleSnapshot); 
+  }
+
+  function handleSnapshot(snapshot) {
+    const links = snapshot.docs.map(doc => {
+      return { id: doc.id, ...doc.data() }
+    })
+    setLinks(links)
+  }
+
+  function renderLinks() {
+    if (isNewPage) {
+      return links
+    }
+    const topLinks = links.slice().sort((l1, l2) => l2.votes.length - l1.votes.length);
+    return topLinks;
+  }
+
+  return (
+    <div>
+      {renderLinks().map((link, index) => 
+        <LinkItem key={link.id} showCount={true} link={link} index={index + 1} />
+      )}
+    </div>);
 }
 
 export default LinkList;
